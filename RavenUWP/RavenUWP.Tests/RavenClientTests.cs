4d7b1953dff5e5d793b8102774e9f9b6f8acc3af
ryanvalentin <ryanvalentin@outlook.com>
2015-08-18 13:14:13 -0700

    Start splitting up and organizing tests

--- RavenUWP/RavenUWP.Tests/RavenClientTests.cs
+++ RavenUWP/RavenUWP.Tests/RavenClientTests.cs
@@ -6,41 +6,13 @@ using System.Threading.Tasks;
 namespace RavenUWP.Tests
 {
     [TestClass]
-    public class RavenUnitTests
+    public class RavenClientTests
     {
         [ClassInitialize]
         public static void RavenTestsInit(TestContext context)
         {
             Dsn dsn = new Dsn(RavenConfig.DSN);
             RavenClient.InitializeAsync(dsn);
-        }
-
-        [TestMethod]
-        public void Test_Create_Valid_Dsn()
-        {
-            try
-            {
-                Dsn dsn = new Dsn("http://public:private@example.com/projectid");
-
-                Assert.AreEqual("public", dsn.PublicKey);
-                Assert.AreEqual("private", dsn.PrivateKey);
-                Assert.AreEqual("projectid", dsn.ProjectID);
-            }
-            catch (ArgumentException ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-        }
-
-        [TestMethod]
-        public void Test_Create_Invalid_Dsn()
-        {
-            Assert.ThrowsException<ArgumentException>(() => new Dsn("invalid_dsn"));
-        }
-
-        public void Test_Create_Null_Dsn()
-        {
-            Assert.ThrowsException<ArgumentNullException>(() => new Dsn(null));
         }
 
         [TestMethod]
